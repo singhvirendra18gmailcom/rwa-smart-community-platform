ac01734ed8528475eb01d27755edfafc1c4c5502
Virendra Singh <singh.virendra18@gmail.com>
2026-09-25 18:00:37 +0530

    Match compact Hindi street light complaint format

--- src/StreetLightInspection.jsx
+++ src/StreetLightInspection.jsx
@@ -94,12 +94,9 @@ function buildComplaintMessage(row, today, issues, config) {
       : `${row.faultyCount} स्ट्रीट लाइट खराब हैं`
 
   return `दिनांक: ${formatMessageDate(today)}
-
-सेवा में,
-${row.agency.agency_name}
+सेवा में ${row.agency.agency_name},
 
 पॉकेट-A, सेक्टर-105 में ${lightText} और यह समस्या ${oldestDays} दिन से लंबित है। कृपया जल्द से जल्द ठीक करवाने की कृपा करें।
-
 अधिक जानकारी के लिए संपर्क करें:
 Supervisor: ${supervisor}
 RWA: ${rwa}
