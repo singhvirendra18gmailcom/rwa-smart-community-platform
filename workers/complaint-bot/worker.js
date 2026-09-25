1c223188c4a74a370397b6c7e85d75bac7567d69
Virendra Singh <singh.virendra18@gmail.com>
2026-09-25 17:58:57 +0530

    Use Hindi locality wording in street light complaint

--- workers/complaint-bot/worker.js
+++ workers/complaint-bot/worker.js
@@ -181,7 +181,7 @@ function streetLightComplaintMessage(agency, inspection, locations, issues, sett
 सेवा में,
 ${agency.agency_name}
 
-Pocket-A, Sector-105 में ${lightText} और यह समस्या ${oldestDays} दिन से लंबित है। कृपया जल्द से जल्द ठीक करवाने की कृपा करें।
+पॉकेट-A, सेक्टर-105 में ${lightText} और यह समस्या ${oldestDays} दिन से लंबित है। कृपया जल्द से जल्द ठीक करवाने की कृपा करें।
 
 अधिक जानकारी के लिए संपर्क करें:
 Supervisor: ${supervisor}
